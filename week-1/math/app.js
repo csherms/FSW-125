a7fd14528458d56613a9e7cbd1d17a71eabe2ed0
csherms <casey.sherman@bryanuniversity.edu>
2021-04-14 14:36:06 -0600

    first commit

---
+++ week-1/math/app.js
@@ -0,0 +1,18 @@
+// Importing functions
+const math = require('./math');
+
+
+// Showing full export of all functions in math.js
+console.log(math);
+
+
+
+// Individual operations
+//Add
+console.log(math.add(10, 2));
+//Sub
+console.log(math.sub(10, 2));
+//Mul
+console.log(math.mul(10, 2));
+//Div
+console.log(math.div(10, 2));
